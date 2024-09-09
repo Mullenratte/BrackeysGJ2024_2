73b3f85b4e2dd4b7f207a8e681999850be6c58c5
Mullenratte <Max.Blanc@web.de>
2024-09-09 14:28:50 +0200

    Ascend & Descend in 0 gravity mode (SPACE / SHIFT)

--- Assets/Mullenratte/Scripts/PlayerMovement.cs
+++ Assets/Mullenratte/Scripts/PlayerMovement.cs
@@ -9,28 +9,36 @@ public class PlayerMovement : MonoBehaviour {
     public Transform orientation;
     public Transform groundCheck;
 
+    float movementSpeedMultiplier = 1f;
     [Header("Ground Movement")]
     public float movementSpeed;
     [SerializeField] float maxVelocityHorizontal;
     [SerializeField] float jumpStrength;
+    [SerializeField] float rbDragGrounded;
     float gravity = 9.81f;
 
     [Header("Air Movement")]
-    [SerializeField, Range(0.01f, 1f)] float airMovementSpeedMultiplier;
+    [SerializeField, Range(0.1f, 1f)] float airMovementSpeedMultiplier;
+    [SerializeField, Range(1f, 2f)] float rbDragAirborne;
+    [SerializeField] float verticalMovementSpeed;
+    [SerializeField] float maxVelocityVertical;
+
 
 
 
     float horizontalInput;
     float verticalInput;
     bool jumpInput;
+    bool descendInput;
     Rigidbody rb;
     Vector3 moveDirection;
 
     private bool isGravitated;
+    private bool isGrounded;
 
     public enum MovementType {
         Gravitated,
-        Airborne
+        ZeroGravity
     }
 
     private MovementType movementType;
@@ -44,55 +52,87 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         InputSystem();
+
+        if (jumpInput) {
+            verticalMovementSpeed = Mathf.Abs(verticalMovementSpeed);
+        }
+        if (descendInput) {
+            verticalMovementSpeed = -Mathf.Abs(verticalMovementSpeed);
+        }
     }
 
     private void FixedUpdate() {
-        if (!isGravitated) {
-            rb.useGravity = false;
-        } else {
-            rb.useGravity = true;
-        }
-        if (movementType == MovementType.Gravitated) {
-            HandleJump();
-        } 
-        if (rb.velocity.y < 0f) {
-            AddJumpingGravity();
-            Debug.Log("gravity affecting rb!");
-        }
-
-
-        Move();
-
-
         if (Physics.Raycast(groundCheck.position, Vector3.down, 5f)) {
-            isGravitated = true;
-        } else {
-            isGravitated = false;
-        }
-        if (Physics.Raycast(groundCheck.position, Vector3.down, 0.1f)) {
             movementType = MovementType.Gravitated;
         } else {
-            movementType = MovementType.Airborne;
+            movementType = MovementType.ZeroGravity;
         }
-        //Debug.DrawRay(groundCheck.position, Vector3.down, Color.red);     
+
+        isGrounded = IsGrounded();
+
+        switch (movementType) {
+            case MovementType.Gravitated:
+                movementSpeedMultiplier = 1f;
+                rb.drag = rbDragGrounded;
+                rb.useGravity = true;
+                if (isGrounded) HandleJump();
+
+                if (rb.velocity.y < 0f) {
+                    AddJumpingGravity();
+                }
+
+                break;
+            case MovementType.ZeroGravity:
+                movementSpeedMultiplier = airMovementSpeedMultiplier;
+                rb.drag = rbDragAirborne;
+                rb.useGravity = false;
+
+                break;
+            default:
+                break;
+        }
+
+        Move();   
     }
 
     private void InputSystem() {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
         jumpInput = Input.GetKey(KeyCode.Space);
+        descendInput = Input.GetKey(KeyCode.LeftShift);
     }
 
     private void Move() {
         float regulationFactor = 1f;    // regulates amount of force that's applied. If maxVelocityHorizontal is exceeded, only a minimal force (5%) will be applied.
-       
+        float regulationFactorVert = 1f;
+
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         if (Mathf.Abs(rb.velocity.x) >= maxVelocityHorizontal || Mathf.Abs(rb.velocity.z) >= maxVelocityHorizontal) {
             regulationFactor = 0.05f;
         }
 
-        rb.AddForce(moveDirection.normalized * movementSpeed * regulationFactor, ForceMode.Force);
+
+
+        if (movementType != MovementType.ZeroGravity) {
+            regulationFactorVert = 0f;
+        } else {
+            if (Mathf.Abs(rb.velocity.y) >= maxVelocityVertical) {
+                regulationFactorVert = 0.05f;
+            }
+            if (!jumpInput && !descendInput) {
+                regulationFactorVert = 0f;
+            }
+        }
+
+        Vector3 movementForce = moveDirection.normalized * movementSpeed * movementSpeedMultiplier * regulationFactor;
+        movementForce += Vector3.up * verticalMovementSpeed * regulationFactorVert;
+
+        rb.AddForce(movementForce, ForceMode.Force);
+    }
+
+    private bool IsGrounded() {
+        return Physics.Raycast(groundCheck.position, Vector3.down, 0.2f);
     }
 
     private void HandleJump() { 
