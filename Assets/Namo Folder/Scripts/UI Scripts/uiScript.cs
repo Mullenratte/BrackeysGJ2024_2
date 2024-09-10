9979310bd09b968249e5729d0a0fdcedcdea208c
namo00 <aymen8bou@gmail.com>
2024-09-10 11:45:23 -0700

    Adding Video Quality an Resolution

--- Assets/Namo Folder/Scripts/UI Scripts/uiScript.cs
+++ Assets/Namo Folder/Scripts/UI Scripts/uiScript.cs
@@ -2,9 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using TMPro;
 public class uiScript : MonoBehaviour
 {
+
+    // ------------- Video Resolution ------------------------------
+
+    Resolution[] resolutions;
+
+    public TMP_Dropdown resolutionDropdown;
+    
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width  + " x "  + resolutions[i].height;
+            options.Add(option);
+
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+
+    // ------------- Loading Scenes ------------------------------
     public void LoadScene(int SceneNumber)
     {
         FindObjectOfType<SoundManager>().PlaySound("Mouse_Click");
@@ -12,21 +39,31 @@ public class uiScript : MonoBehaviour
         SceneManager.LoadScene(SceneNumber);
     }
 
-
+    // ------------- EXIT Game ------------------------------
     public void Exit()
     {
         Application.Quit();
     }
 
+    // ------------- Pause & Resume ------------------------------
     public void PauseAndResume(int timeScale)
     {
         Time.timeScale = timeScale;
         FindObjectOfType<SoundManager>().PlaySound("Mouse_Click");
     }
 
+
+    // ------------- GAME OVER ------------------------------
     public void GameOver()
     {
         FindObjectOfType<SoundManager>().PlaySound("You_Lost");
+    }
+
+
+    // ------------- Video Quality (LOW,..., HIGH) ------------------------------
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
     
    
